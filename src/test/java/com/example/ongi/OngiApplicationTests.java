8b6c8b6a14f4986796e05bd886171c3f306b6658
chaeyeon0130 <k84292330@gmail.com>
2025-02-06 20:29:11 +0900

    init

---
+++ src/test/java/com/example/ongi/OngiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ongi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
